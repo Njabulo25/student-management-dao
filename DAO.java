90cd17a89682ca65987653797596394db8866134
Njabulo Rikhotso <57141677+Njabulo25@users.noreply.github.com>
2025-12-26 16:21:31 +0200

    Update DAO.java
    Made changes on commenting

--- DAO.java
+++ DAO.java
@@ -3,12 +3,23 @@ package za.ac.tut.bl;
 
 import java.util.List;
 
+/**
+ * Generic DAO interface defining standard CRUD operations for any entity type.
+ *
+ * @param <T> the entity type
+ */
+
 public interface DAO<T>
 {
     
     T get(Integer code);
+    
     List<T> getAll();
+    
     boolean add(T t);
+    
     boolean update(T t);
+    
     boolean delete(T t);
 }
+
